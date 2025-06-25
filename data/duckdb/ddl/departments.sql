b4e17c50987dc6795afd2ac99d4caf1d317eed9f
Rafael Noriega <rafael.noriega@cencosud.cl>
2025-06-24 20:00:33 -0400

    feat: create data folder, requirements

---
+++ data/duckdb/ddl/departments.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS departments (
+    id INTEGER, -- Id of the department
+    department VARCHAR -- Name of the department
+);
---
+++ data/duckdb/ddl/jobs.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS jobs (
+    id INTEGER, -- Id of the job
+    job VARCHAR -- Name of the job
+);
